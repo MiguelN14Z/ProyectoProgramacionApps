54f3a419c13156eef87e73ca95f88223e4f7d1c3
migue <migue@192.168.124.235>
2024-11-28 14:49:28 -0500

    proyecto

---
+++ src/application/Esce2Controller.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.DatePicker;
+
+import javafx.scene.control.ChoiceBox;
+
+public class Esce2Controller {
+	@FXML
+	private TextField dni;
+	@FXML
+	private TextField nombre;
+	@FXML
+	private TextField cargp;
+	@FXML
+	private TextField salario;
+	@FXML
+	private ChoiceBox jefe;
+	@FXML
+	private ChoiceBox depa;
+	@FXML
+	private DatePicker nacimiento;
+	@FXML
+	private DatePicker incorporacion;
+	@FXML
+	private ChoiceBox sexo;
+	@FXML
+	private TextField comision;
+
+}
